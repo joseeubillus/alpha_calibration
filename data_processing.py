be092b0bc2bbdba085cdb4ef2b5a758d3fb43a6d
José Eduardo Ubillús Alcivar <joseeduardou98@gmail.com>
2023-02-06 01:26:26 -0600

    Ready to deploy, trial one

--- data_processing.py
+++ data_processing.py
@@ -19,36 +19,35 @@ from sklearn.linear_model import LinearRegression
 
 # Inputs
 
-os.chdir('C:/Users/josee/Box/2022-2023 GRA/Sand Tank Experiments/2022_Ubillus_Experiments/01-05-2023_Ripples_2nd_trial/Data_processed')
+os.chdir('C:/Users/Jose/Box/2022-2023 GRA/Sand Tank Experiments/2022-4-16-P337-P170-ripples2/Processed-Data/')
 
-pixel_dim = 0.0256 # cm
-poro = 0.397
+pixel_dim = 0.0234 # cm
+poro = 0.383
 thick = 1.8
-inj_rate = 0.2
+inj_rate = 0.185
 
 # Drainage images
 
-start_image_dra = 2 # NWP first seen in tank
-end_image_dra = 693 # domain breaktrough
-interval_dra = (end_image_dra-start_image_dra)//50
+start_image_dra = 1 # NWP first seen in tank
+end_image_dra =111 # domain breaktrough
+interval_dra = 2
 
 # Redistribution images 
 
 start_image_red = 1 # NWP first seen in tank
-end_image_red = 1463 # domain breaktrough
-interval_red = (end_image_dra-start_image_dra)//10
-
+end_image_red = 94 # domain breaktrough
+interval_red = 10
 
 mask_file=scipy.io.loadmat('BW.mat')
 mask=mask_file.get('BW')
 
-tiff_file_wet='Drainage_processed/DRA1.tif'
+tiff_file_wet='Drainage/DRA1.tif'
 
 # Whole domain saturation
-image_sequence_dra = np.arange(start=start_image_dra,stop=end_image_dra,step=interval_dra,dtype=int)
+image_sequence_dra = np.arange(start=start_image_dra,stop=end_image_dra+1,step=interval_dra,dtype=int)
 num_image_dra = len(image_sequence_dra)
 
-image_sequence_red = np.arange(start=start_image_red,stop=end_image_red,step=interval_red,dtype=int)
+image_sequence_red = np.arange(start=start_image_red,stop=end_image_red+1,step=interval_red,dtype=int)
 num_image_red = len(image_sequence_red)
 
 dim1, dim2 = mask.shape
@@ -62,21 +61,21 @@ Iw = cv2.imread(tiff_file_wet,-1)
 # Drainage
 counter = 0 
 for i in image_sequence_dra:
-    I_dra=cv2.imread("Drainage_processed/DRA"+str(i)+'.tif',-1)
+    I_dra=cv2.imread("Drainage/DRA"+str(i)+'.tif',-1)
     Snw_dra[:,:,counter]=np.log(Iw)-np.log(I_dra)
     counter=counter+1
     
 # Redistribution
 counter = 0 
 for i in image_sequence_red:
-    I_red=cv2.imread("Redistribution_processed/RED"+str(i)+'.tif',-1)
+    I_red=cv2.imread("Redistribution/IMB"+str(i)+'.tif',-1)
     Snw_red[:,:,counter]=np.log(Iw)-np.log(I_red)
     counter=counter+1
     
 time_DRA = []
 
 for i in image_sequence_dra:
-    path_file='C:/Users/josee/Box/2022-2023 GRA/Sand Tank Experiments/2022_Ubillus_Experiments/01-05-2023_Ripples_2nd_trial/Drainage/'+'DRATS'+str(i)+".fit"
+    path_file='C:/Users/Jose/Box/2022-2023 GRA/Sand Tank Experiments/2022-4-16-P337-P170-ripples2/Drainage/'+'DRATS'+str(i)+".fit"
     timestamp=os.path.getmtime(path_file)
     datestamp=datetime.fromtimestamp(timestamp)
     time_DRA.append(datestamp)
@@ -108,24 +107,24 @@ Vnw_computed = np.sum(Snw_field_dra*(pixel_dim**2)*thick*poro,axis=(0,1))
 
 # Plots 
 ## Snw vs LnIw - LnI
-fig, ax = plt.subplots(figsize=(1,1),dpi=150)
+fig, ax = plt.subplots(figsize=(3.54,3.54),dpi=600)
 
 ax.scatter(X,y,color='blue',marker='x',linewidths=0.5,alpha=0.5,label='Data')
 ax.plot(X,alpha_matched*X,color='red',linestyle='dashdot',label='Linear model')
-ax.set_title('Alpha calibration - Redistribution')
+ax.set_title('Alpha calibration')
 ax.set_xlabel('ln(Iw)-ln(I)')
 ax.set_ylabel('Snw')
-ax.text(np.max(X)+10000,np.mean(y)+0.005,'Alpha domain: '+str(np.round(alpha_matched,10)))
+ax.text(np.max(X)+10000,np.mean(y)+0.0005,'Alpha domain: '+str(np.round(alpha_matched,10)))
 ax.text(np.max(X)+10000,np.mean(y),'Alpha pixel: '+str(np.round(alpha_pixel,4)))
-ax.set_xlim(0,np.max(X))
+ax.set_xlim(0,np.max(X)+2)
 ax.set_ylim(0,np.max(y))
 ax.legend()
 plt.ticklabel_format(axis='x',style='scientific',scilimits=(4,4))
-plt.tight_layout()
+#plt.tight_layout()
 plt.show()
 
 ## Vnw computed vs Vnw theoretical
-fig, ax = plt.subplots(figsize=(1,1),dpi=150)
+fig, ax = plt.subplots(figsize=(3.54,3.54),dpi=600)
 
 ax.scatter(Vnw,Vnw_computed,marker='o',alpha=0.5,linewidths=0.5,label='Data')
 ax.axline([0,0],[1,1],linestyle='--',color='black',linewidth=1)
@@ -135,16 +134,16 @@ ax.set_xlabel('Theoretical Vnw (ml)')
 ax.set_xlim(0,np.max(Vnw))
 ax.set_ylim(0,np.max(Vnw_computed))
 ax.legend()
-plt.tight_layout()
+#plt.tight_layout()
 plt.show()
 
 ## Discrepancy Vnw
-fig, ax = plt.subplots(figsize=(1,1),dpi=150)
+fig, ax = plt.subplots(figsize=(3.54,3.54),dpi=600)
 
 ax.scatter(np.arange(1,len(Vnw_computed)+1,1),Vnw_computed-Vnw,marker='o',alpha=0.5,linewidths=0.5,color='green')
 ax.set_title('Discrepancy in NWP Volume')
 ax.set_ylabel('Vnw (ml)')
-plt.tight_layout()
+#plt.tight_layout()
 plt.show()
 
 ## Saturation field 
@@ -152,7 +151,7 @@ def plot_map(Sof,tiff_file_drainage,title):
     
     img=plt.imread(tiff_file_drainage)
     
-    fig,ax = plt.subplots(figsize=(1,1),dpi=150)
+    fig,ax = plt.subplots(figsize=(3.54,3.54),dpi=600)
     
     img=ax.imshow(img,cmap='gray')
     Sof_field=np.ma.masked_where(Sof<0.05,Sof)
@@ -175,7 +174,7 @@ def plot_map(Sof,tiff_file_drainage,title):
         labelleft=False) # labels along the bottom edge are off
     
     plt.title(title,pad=10)
-    plt.tight_layout()
+ #   plt.tight_layout()
 
     return plt.show()
 
@@ -192,19 +191,23 @@ def video_generator (imgs,tiff_file_drainage):
     for i in range(dim3):
         frames.append([plt.imshow(Sof_field[:,:,i],cmap='viridis',interpolation='nearest',vmin=0.1,vmax=1,animated=True)])
 
-    ani = animation.ArtistAnimation(fig,frames,interval=50,blit=True,repeat_delay=1000)
+    ani = animation.ArtistAnimation(fig,frames,interval=3000,blit=True,repeat_delay=5000)
 
-    #plt.colorbar(fig,ax=ax,orientation='vertical',shrink=0.8)
+    plt.colorbar(orientation='vertical',shrink=0.8)
+    
+    f = r"C:/Users/Jose/Box/2022-2023 GRA/Sand Tank Experiments/2022-4-16-P337-P170-ripples2/Processed-Data/animation_ripples_337_170.gif" 
+    writergif = animation.PillowWriter(fps=5) 
+    ani.save(f, writer=writergif)
 
-    return plt.show()
+    return 
 
 
 
 
-plot_map(Snw_field_dra[:,:,num_image_dra-1],tiff_file_wet,'Drainage')
+plot_map(Snw_field_dra[:,:,-1],tiff_file_wet,'Drainage')
 
 plot_map(Snw_field_red[:,:,1],tiff_file_wet,'End of Drainage')
 
-plot_map(Snw_field_red[:,:,num_image_red-1],tiff_file_wet,'End of Redistribution')
+plot_map(Snw_field_red[:,:,-1],tiff_file_wet,'End of Redistribution')
 
 video_generator (Snw_field_dra,tiff_file_wet)
