818a557a8290f324c9cb395ad0a914d8e3d2dd7b
José Eduardo Ubillús Alcivar <joseeduardou98@gmail.com>
2023-02-06 01:27:04 -0600

    ready to deploy

---
+++ animation_repo.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(page_title='Animation Repository',
+                   page_icon='♣',
+                   layout='centered',
+                   initial_sidebar_state='collapsed')
+
+st.image('GCCC_header.png',use_column_width=True)
+st.markdown("<h1 style='text-align: center; color: red;'>Animation Repository</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: black;'>Ripples bedforms</h1>", unsafe_allow_html=True)
+
+st.text('Developed by: Jose Ubillus and Hailun Ni')
+st.text('')
+st.subheader('Ripples in phase')
+st.subheader('Beads Contrast: P337 - P230')
+st.image('https://media.giphy.com/media/apis0c44ouo0Fmgns1/giphy.gif',caption='By: Hailun Ni',use_column_width=True)
+st.subheader('Beads Contrast: P337 - P170')
+st.image('https://media.giphy.com/media/WBlSadrECZ8s2aTLht/giphy.gif',caption='By: Hailun Ni',use_column_width=True)
+st.subheader('Beads Contrast: P337 - P140')
+st.image('https://media.giphy.com/media/fXgagCqo1RxsCBBRPu/giphy.gif',caption='By: Jose Ubillus',use_column_width=True)
+st.subheader('Beads Contrast: P337 - P080')
+st.image('https://media.giphy.com/media/Gwj4XPuq5woU1LgepH/giphy.gif',caption='By: Jose Ubillus',use_column_width=True)
