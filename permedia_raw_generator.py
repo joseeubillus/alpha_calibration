e72c6e38d78bf2ec9c3c00c27f3f1317cf877f7c
joseeubillus <joseeduardou98@gmail.com>
2023-05-01 12:55:51 -0500

    generate raw done

--- permedia_raw_generator.py
+++ permedia_raw_generator.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 
 # path to segmented image
-path = 'C:/Users/josee/Box/2022-2023 GRA/Permedia_Ubillus/test_3d/test_ml_ij.tif'
+path = 'C:/Users/BEGuser/Box/2022-2023 GRA/Permedia_Ubillus/test_3d/test_ml_ij.tif'
 
 # read image
 img = cv2.imread(path, -1)
@@ -20,14 +20,19 @@ vol_3d = np.repeat(img[:, :, np.newaxis], 10, axis=2)
 model_1d = np.flipud(np.rot90(img,1))
 model_3d = np.flipud(np.rot90(vol_3d,1))
 
+# Shape of both arrays
+dim1, dim2 = model_1d.shape
+dim3, dim4, dim5 = model_3d.shape
+
 # Flatten 2D and 3D arrays
 flattened_1d = model_1d.flatten('F')
 flattened_3d = model_3d.flatten('F')
 
 # Create txt file for 1D array
-filename = 'model.raw'
+filename = 'model_1D.raw'
 with open(filename, 'w') as f:
     f.write('# .raw version 0.700\n')
+    f.write('extents: ' + str(dim1) + ' ' + str(1) + ' ' + str(dim2) + '\n')
     f.write('origin: 0 0 0\n')
     f.write('cellsize: 0.002 0.02 0.002\n')
     f.write('rotation: 0\n')
@@ -36,4 +41,13 @@ with open(filename, 'w') as f:
         f.write("%s\n" % item)
 
 # Create txt file for 3D array
-# 1D array
+filename = 'model_3D.raw'
+with open(filename, 'w') as f:
+    f.write('# .raw version 0.700\n')
+    f.write('extents: ' + str(dim3) + ' ' + str(dim4) + ' ' + str(dim5) + '\n')
+    f.write('origin: 0 0 0\n')
+    f.write('cellsize: 0.002 0.002 0.002\n')
+    f.write('rotation: 0\n')
+    f.write('data: ascii\n')
+    for item in flattened_3d:
+        f.write("%s\n" % item)
